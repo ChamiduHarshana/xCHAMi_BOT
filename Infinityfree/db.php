4c5bdc54523336f6118d22e7d926331e6644c38f
chamidu <151662458+ChamiduHarshana@users.noreply.github.com>
2026-04-07 16:11:26 +0530

    Update db.php

--- Infinityfree/db.php
+++ Infinityfree/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "sql201.infinityfree.com"; // vPanel එකේ තියෙන Hostname එක
-$user = "if0_41598040";      // Username
-$pass = "8YcIeaow39fyRxN";    // Password
-$db   = "if0_41598040_My_Bot";   // Database Name
+$host = "xxxxx.infinityfree.com"; // vPanel එකේ තියෙන Hostname එක
+$user = "xxxxxxxx";      // Username
+$pass = "xxxxx";    // Password
+$db   = "xxxx_My_Bot";   // Database Name
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
